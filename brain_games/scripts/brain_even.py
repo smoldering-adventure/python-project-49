c9a5ddb3680b9c303166d840ab8dc5609187015e
Smoldering <m.p.w.200032@gmail.com>
2025-02-25 15:12:57 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,27 +1,24 @@
 from random import random
+
 import prompt  # type: ignore
 
 # Приветствие и выеснение имени пользователя
-def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print("Hello, " + name + "!")
-
-if __name__ == "__main__":
-    main()
+print("Welcome to the Brain Games!")
+name = prompt.string('May I have your name? ')
+print("Hello, " + name + "!")
 
 # Правила
 print('Answer "yes" if the number is even, otherwise answer "no".')
 
 n = 0
 
-# Игра, где нужно ответить 3 раза подряд правильно
+# Игра, где нужно ответить 3 раза подряд правильно определить чётность
 while n < 3:
     # Генерация числа и выяснение ответа пользователя
     number = random()
-    user_answer = prompt.string("Question: {number}\nYour answer: ")
+    user_answer = prompt.string(f"Question: {number}\nYour answer: ")
     
-    # Определение ответа
+    # Определение чётности
     if number // 2 == 0:
         answer = 'yes'
     else:
@@ -32,8 +29,9 @@ while n < 3:
         print('Correct!')
         n += 1
     else:
-        print("'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'.\nLet's try again, {name}!")
+        print(f"'{user_answer}' is wrong answer ;(. Correct answer was "
+              f"'{answer}'.\nLet's try again, {name}!")
         n = 0  # Обнуление счётчика
 
 # Победа
-print("Congratulations, {name}!")
+print(f"Congratulations, {name}!")
