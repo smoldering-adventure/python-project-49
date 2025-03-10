90e57dc66b62e8d58f7cbc765150043c52bee697
Smoldering <m.p.w.200032@gmail.com>
2025-03-10 10:49:38 +0300

    add brain_games

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 install:
 	uv sync
 
+brain-games:
+	uv run brain-games
+
 brain-even:
 	uv run brain-even
 
---
+++ brain_games/scripts/brain-games.py
@@ -0,0 +1,8 @@
+from brain_games.cli import welcome_user
+
+def main():
+    name = welcome_user()
+    return name
+
+if __name__ == "__main__":
+    main()
